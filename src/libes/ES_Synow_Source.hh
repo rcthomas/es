d497db32b24aec424f450c8bb7ccf535717ef5df
R. C. Thomas <rcthomas@lbl.gov>
2010-10-17 10:07:33 -0700

    Update in prep for release.

--- src/libes/ES_Synow_Source.hh
+++ src/libes/ES_Synow_Source.hh
@@ -2,23 +2,28 @@
 // File    : ES_Synow_Source.hh
 // ----------------------------
 //
-// Copyright (C) 2010 Rollin C. Thomas <rcthomas@lbl.gov>
-// 
-// This file is part of ES.
-// 
-// ES is free software: you can redistribute it and/or modify it under the
-// terms of the GNU General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or (at your
-// option) any later version.
-// 
-// ES is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-// for more details.
-// 
-// You should have received a copy of the GNU General Public License along
-// with ES.  If not, see <http://www.gnu.org/licenses/>.
-//  
+// ES: Elementary Supernova Spectrum Synthesis, Copyright (c) 2010, The
+// Regents of the University of California, through Lawrence Berkeley
+// National Laboratory (subject to receipt of any required approvals
+// from the U.S. Dept. of Energy). All rights reserved.
+//
+// If you have questions about your rights to use or distribute this
+// software, please contact Berkeley Lab's Technology Transfer
+// Department at TTD@lbl.gov.
+//
+// NOTICE. This software was developed under partial funding from the
+// U.S. Department of Energy. As such, the U.S. Government has been
+// granted for itself and others acting on its behalf a paid-up,
+// nonexclusive, irrevocable, worldwide license in the Software to
+// reproduce, prepare derivative works, and perform publicly and display
+// publicly. Beginning five (5) years after the date permission to
+// assert copyright is obtained from the U.S. Department of Energy, and
+// subject to any subsequent five (5) year renewals, the U.S. Government
+// is granted for itself and others acting on its behalf a paid-up,
+// nonexclusive, irrevocable, worldwide license in the Software to
+// reproduce, prepare derivative works, distribute copies to the public,
+// perform publicly and display publicly, and to permit others to do so. 
+//
 
 #ifndef ES__SYNOW__SOURCE
 #define ES__SYNOW__SOURCE
