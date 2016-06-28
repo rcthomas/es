6685b19dea4efe7e73611554f3ef739309995c59
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2016-06-27 19:33:59 -0500

    changes in Synow/Source for using c and 1/c constants; changes to handle user based velocity grid

--- src/libes/ES_Synow_Source.cc
+++ src/libes/ES_Synow_Source.cc
@@ -29,6 +29,7 @@
 #include "ES_Synow_Grid.hh"
 #include "ES_Synow_Setup.hh"
 #include "ES_Blackbody.hh"
+#include "ES_Constants.hh"
 
 #ifdef _OPENMP
 #include "omp.h"
@@ -72,39 +73,76 @@ void ES::Synow::Source::operator() ( const ES::Synow::Setup& setup )
     {
 
         double v = _grid->v[ iv ];
-        double mu_crit = iv > 0 ? sqrt( 1.0 - v_phot * v_phot / v / v ) : 0.0;
+		if (_grid->v[ iv] >= setup.v_phot)
+		{
+		    double mu_crit = iv > 0 ? sqrt( 1.0 - v_phot * v_phot / v / v ) : 0.0;
 
-        int    offset, i;
-        double dmu, mu_init;
+		    int    offset, i;
+		    double dmu, mu_init;
 
-        // Photosphere.
+		    // Photosphere.
 
-        dmu     = ( 1.0 - mu_crit ) / double( _mu_size );
-        mu_init = 1.0 - 0.5 * dmu;
+		    dmu     = ( 1.0 - mu_crit ) / double( _mu_size );
+		    mu_init = 1.0 - 0.5 * dmu;
 
-        offset = iv * _mu_size * 2;
-        for( int im = 0; im < _mu_size; ++ im )
-        {
-            i = offset + im;
-            _mu   [ i ] = mu_init - im * dmu;
-            _dmu  [ i ] = dmu;
-            _shift[ i ] = 1.0 / ( 1.0 + ( v * _mu[ i ] - sqrt( v * v * ( _mu[ i ] * _mu[ i ] - 1.0 ) + v_phot  * v_phot  ) ) / 299.792 );
-        }
+		    offset = iv * _mu_size * 2;
+		    for( int im = 0; im < _mu_size; ++ im )
+		    {
+		        i = offset + im;
+		        _mu   [ i ] = mu_init - im * dmu;
+		        _dmu  [ i ] = dmu;
+		        _shift[ i ] = 1.0 / ( 1.0 + ( v * _mu[ i ] - sqrt( v * v * ( _mu[ i ] * _mu[ i ] - 1.0 ) + v_phot  * v_phot  ) ) * ES::_inv_c );
+		    }
 
-        // Sky.
+		    // Sky.
 
-        dmu     = ( mu_crit + 1.0 ) / double( _mu_size );
-        mu_init = mu_crit - 0.5 * dmu;
+		    dmu     = ( mu_crit + 1.0 ) / double( _mu_size );
+		    mu_init = mu_crit - 0.5 * dmu;
 
-        offset = iv * _mu_size * 2 + _mu_size;
-        for( int im = 0; im < _mu_size; ++ im )
-        {
-            i = offset + im;
-            _mu   [ i ] = mu_init - im * dmu;
-            _dmu  [ i ] = dmu;
-            _shift[ i ] = 1.0 / ( 1.0 + ( v * _mu[ i ] + sqrt( v * v * ( _mu[ i ] * _mu[ i ] - 1.0 ) + v_outer * v_outer ) ) / 299.792 );
-        }
+		    offset = iv * _mu_size * 2 + _mu_size;
+		    for( int im = 0; im < _mu_size; ++ im )
+		    {
+		        i = offset + im;
+		        _mu   [ i ] = mu_init - im * dmu;
+		        _dmu  [ i ] = dmu;
+		        _shift[ i ] = 1.0 / ( 1.0 + ( v * _mu[ i ] + sqrt( v * v * ( _mu[ i ] * _mu[ i ] - 1.0 ) + v_outer * v_outer ) ) * ES::_inv_c );
+		    }
+		}
+		else
+		{
+		    double mu_crit = 0.0;
 
+		    int    offset, i;
+		    double dmu, mu_init;
+
+		    // Photosphere.
+
+		    dmu     = ( 1.0 - mu_crit ) / double( _mu_size );
+		    mu_init = 1.0 - 0.5 * dmu;
+
+		    offset = iv * _mu_size * 2;
+		    for( int im = 0; im < _mu_size; ++ im )
+		    {
+		        i = offset + im;
+		        _mu   [ i ] = mu_init - im * dmu;
+		        _dmu  [ i ] = dmu;
+		        _shift[ i ] = 1.0 / ( 1.0 + ( v * _mu[ i ] - sqrt( v * v * ( _mu[ i ] * _mu[ i ] - 1.0 ) + v_phot  * v_phot  ) ) * ES::_inv_c );
+		    }
+
+		    // Sky.
+
+		    dmu     = ( mu_crit + 1.0 ) / double( _mu_size );
+		    mu_init = mu_crit - 0.5 * dmu;
+
+		    offset = iv * _mu_size * 2 + _mu_size;
+		    for( int im = 0; im < _mu_size; ++ im )
+		    {
+		        i = offset + im;
+		        _mu   [ i ] = mu_init - im * dmu;
+		        _dmu  [ i ] = dmu;
+		        _shift[ i ] = 1.0 / ( 1.0 + ( v * _mu[ i ] + sqrt( v * v * ( _mu[ i ] * _mu[ i ] - 1.0 ) + v_outer * v_outer ) ) * ES::_inv_c );
+		    }
+		}
     }
 
     // Adjust photosphere SED table as needed.
@@ -140,7 +178,10 @@ void ES::Synow::Source::operator() ( const ES::Synow::Setup& setup )
                 {
                     d  = ( _grid->wl[ iw ] / _grid->wl[ ib ] - 1.0 ) * 299.792;
                     vd = sqrt( v * v + d * d - 2.0 * v * d * _mu[ i ] );
-                    il = int( ( vd - v_phot ) / v_step );
+					if (_grid->v_user)
+			        	il = int( ( vd - _grid->v[0] ) / v_step );
+					else
+			        	il = int( ( vd - v_phot ) / v_step );
                     iu = il + 1;
                     cl = ( _grid->v[ iu ] - vd ) / v_step;
                     cu = 1.0 - cl;
