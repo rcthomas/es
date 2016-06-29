4a22dd2b45f88455622a08ad3fd7ad9b1ef60a41
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2016-06-29 16:50:50 -0500

    add compiler directive to handle the most common alternate path to fitsio header, setup in configure

--- src/libes/ES_Spectrum.cc
+++ src/libes/ES_Spectrum.cc
@@ -27,8 +27,11 @@
 
 #include "ES_Spectrum.hh"
 #include "ES_Exception.hh"
-
+#ifdef FITSIO_CFITSIO
+#include "cfitsio/fitsio.h"
+#else
 #include "fitsio.h"
+#endif
 
 #include <cmath>
 #include <limits>
